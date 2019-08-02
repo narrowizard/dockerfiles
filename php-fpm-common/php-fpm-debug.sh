ee3bf961935f5833e54e820c7d4a1a024a294b55
narro <501xs05@163.com>
2019-08-02 14:50:02 +0800

    enable fpm status

--- php-fpm-common/php-fpm-debug.sh
+++ php-fpm-common/php-fpm-debug.sh
@@ -1,6 +1,3 @@
 apk update
 apk add fcgi
-SCRIPT_NAME=/status \
-SCRIPT_FILENAME=/status \
-REQUEST_METHOD=GET \
-cgi-fcgi -bind -connect 127.0.0.1:9000
+SCRIPT_NAME=/status SCRIPT_FILENAME=/status QUERY_STRING=full REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000
